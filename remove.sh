6701a3963b092d9e30e903812932b5549e44628a
rbasry29-blip <rbasry29@gmail.com>
2025-11-29 20:07:50 +0600

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,6 @@
+printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
+printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
+printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
+printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
+proot-distro remove ubuntu
+apt remove proot-distro x11-repo termux-x11-nightly -y
