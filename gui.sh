bf02072f13290e50a7cd74c70ce3a5626a6e937a
rbasry29-blip <rbasry29@gmail.com>
2025-11-29 13:42:23 +0600

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+ #!/bin/bash
 g='\e[92m'
 b='\e[34m'
 r='\e[0m'
+
 a(){
 
 clear
@@ -9,24 +10,33 @@ printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${g}Updating and  Installing packages (xubuntu-desktop and dbus-x11)  ${r}\n"
-proot-distro login ubuntu -- b(){
-apt update -y 
-apt install xubuntu-desktop dbus-x11 -y
-printf "${b}[${g}*${b}]${g}Adding user " ubuntu ${r}\n"
-adduser ubuntu
+printf "${b}[${g}*${b}]${g} Updating and Installing packages (xubuntu-desktop and dbus-x11) ${r}\n"
+
+# Login to Ubuntu and run commands sequentially
+proot-distro login ubuntu -- bash -c "apt update -y && apt install xubuntu-desktop dbus-x11 -y && printf '${b}[${g}*${b}]${g} Adding user ubuntu ${r}\n' && adduser --disabled-password --gecos '' ubuntu"
+
+# Create gui launcher inside Ubuntu
+proot-distro login ubuntu -- bash -c "cat > /usr/bin/gui << 'EOF'
+#!/bin/bash
+export DISPLAY=:0
+dbus-launch
+xfce4-session &
+EOF
+chmod +x /usr/bin/gui"
+
 clear
 printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${g}Finishing...
-Everthing is good!
-You can launsh ubuntu via
-termux-x11 using command -
-1.server 
-2.ubuntu
-3.gui
+printf "${b}[${g}*${b}]${g} Finishing...
+Everything is good!
+You can launch Ubuntu via termux-x11 using commands:
+1. server
+2. ubuntu
+3. gui
 Enjoy! ${r}\n"
-b }
+
+}
+
 a
