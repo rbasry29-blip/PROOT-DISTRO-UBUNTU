6abb1dea5a13da7bade7913f835cccaf4fccc6de
rbasry29-blip <rbasry29@gmail.com>
2025-11-29 13:26:53 +0600

    Create gui.sh

---
+++ gui.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+g='\e[92m'
+b='\e[34m'
+r='\e[0m'
+a(){
+
+clear
+printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
+printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
+printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
+printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
+printf "${b}[${g}*${b}]${g}Updating and  Installing packages (xubuntu-desktop and dbus-x11)  ${r}\n"
+proot-distro login ubuntu -- b(){
+apt update -y 
+apt install xubuntu-desktop dbus-x11 -y
