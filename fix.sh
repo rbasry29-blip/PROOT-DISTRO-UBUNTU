778b0572c6fe7992191e8ced604359c7bf1de5f2
rbasry29-blip <rbasry29@gmail.com>
2025-12-02 20:08:01 +0600

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,7 +1,1 @@
-proot-distro login ubuntu -- rm -f /var/lib/dpkg/info/elementary-xfce-icon-theme.postinst
-
-proot-distro login ubuntu -- dpkg --force-all --configure elementary-xfce-icon-theme
-
-proot-distro login ubuntu -- dpkg --configure -a
-
-proot-distro login ubuntu -- apt --fix-broken install 
+ 
