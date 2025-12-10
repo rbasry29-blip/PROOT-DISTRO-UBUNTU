6305fc26a9353940c0ecdf36ae41c7c83f23ad40
linuxdevoloper <rbasry29@gmail.com>
2025-12-10 15:28:11 +0600

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -340,8 +340,6 @@ printf "\e[1;33m    _  _ ___  _  _ _  _ ___ _  _\n"
 printf "\e[1;96m    |  | |__] |  | |\\ |  |  |  |\n"
 printf "\e[1;92m    |__| |__] |__| | \\|  |  |__|\n"
 printf "\e[1;92m     PROOT-DISTRO-UBUNTU\n\n\e[0m"
-printf "${b}[${g}*${b}]${g} Adding user ubuntu ${r}\n" 
-proot-distro login ubuntu -- adduser ubuntu
 
 printf "${b}[${g}*${b}]${g} Creating launchers  ${r}\n"
 
